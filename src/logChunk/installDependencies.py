2cbcb2317ab091131e5a8eba16a4876e0ae93aae
Yagnik Suchak <yagnik4@gmail.com>
2016-06-09 02:45:14 -0700

    first version of installDependencies python file

---
+++ src/logChunk/installDependencies.py
@@ -0,0 +1,6 @@
+import os
+os.system("pip install PyYAML")
+os.system("pip install nltk")
+os.system("python -m nltk.downloader stopwords")
+os.system("python -m nltk.downloader wordnet")
+os.system("pip install psycopg2")
